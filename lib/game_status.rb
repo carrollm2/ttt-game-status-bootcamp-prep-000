6712cd109e6ac599b5a9eb70b729f3af364c514d
Michael Carroll <carrollm2@gmail.com>
2019-03-05 12:56:18 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -60,7 +60,8 @@ end
 
 def winner(board)
   winning_combination = won?(board)
-  if won?(board)
+  #if won?(board)
+  if winning_combination
     return board[winning_combination[0]]
   else
     return nil
